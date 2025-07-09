b03a94e4e4986cd343a52d771b3a30e47c8d00b6
Fiona <fiona.longkutoy@student.president.ac.id>
2025-07-09 21:46:17 +0700

    Initial commit: Dockerized static portfolio

---
+++ public/config.js
@@ -0,0 +1,6 @@
+const emailConfig = {
+  PUBLIC_KEY: '4df7jefmtFJbjUTSL',
+  SERVICE_ID: 'service_1wqd7zj',
+  TEMPLATE_ID: 'template_vcxrpd4',
+  RECAPTCHA_SITE_KEY: '6LeEhnsrAAAAACPSFVoODLMU1Dfh65pjuebgwiIT'
+};
